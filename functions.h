025f36a3f3e423da7e8a5158d3cc9dbb06f13194
Bishwo Dahal <bishwodahal123@gmail.com>
2021-06-30 13:37:37 +0545

    changed functions.h style NOO OOP here

--- functions.h
+++ functions.h
@@ -5,64 +5,42 @@
 #include <string>
 #include <string.h>
 
-class Functions
+namespace Functions
 {
-private:
-    /* data */
-public:
-    Functions(/* args */);
-    ~Functions();
-
-    static std::string getCurrentTime();
-    static std::string toUpperCase(std::string param);
-    static std::string toLowerCase(std::string param);
-    static bool compareCaseInsensitive(std::string firstParam, std::string secondParam);
-    namespace owww
+    std::string getCurrentTime()
     {
-        int usedNamespace;
+        std::string date = "";
+        time_t now = time(0);
+        char *currentTime = ctime(&now);
+
+        date = currentTime;
+        return date;
     }
-};
-
-Functions::Functions(/* args */)
-{
-}
-
-Functions::~Functions()
-{
-}
-std::string Functions::getCurrentTime()
-{
-    std::string date = "";
-    time_t now = time(0);
-    char *currentTime = ctime(&now);
-
-    date = currentTime;
-    return date;
-}
-std::string Functions::toUpperCase(std::string param)
-{
-    std::string answer = "";
-    for (int pLength = 0; pLength < param.length(); pLength++)
+    std::string toUpperCase(std::string param)
     {
-        answer += std::toupper(param.at(pLength));
+        std::string answer = "";
+        for (int pLength = 0; pLength < param.length(); pLength++)
+        {
+            answer += std::toupper(param.at(pLength));
+        }
+        return answer;
     }
-    return answer;
-}
-std::string Functions::toLowerCase(std::string param)
-{
-    std::string answer = "";
-    for (int pLength = 0; pLength < param.length(); pLength++)
+    std::string toLowerCase(std::string param)
     {
-        answer += std::tolower(param.at(pLength));
+        std::string answer = "";
+        for (int pLength = 0; pLength < param.length(); pLength++)
+        {
+            answer += std::tolower(param.at(pLength));
+        }
+        return answer;
     }
-    return answer;
-}
-bool Functions::compareCaseInsensitive(std::string firstParam, std::string secondParam)
-{
-    if (toUpperCase(firstParam) == toUpperCase(secondParam))
-        return true;
+    bool compareCaseInsensitive(std::string firstParam, std::string secondParam)
+    {
+        if (toUpperCase(firstParam) == toUpperCase(secondParam))
+            return true;
 
-    return false;
-}
+        return false;
+    }
 
+}
 #endif
